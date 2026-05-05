ab996c7e7e6f3bd5b040a6f622ffb5a3e4ab7e67
Felipe Ferrer Sorrilha <145513328+FelipeSorrilha@users.noreply.github.com>
2026-05-05 20:38:29 -0300

    começo do analisador sintático

---
+++ analisadorsintatico/AnalisadorSintatico.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analisadorsintatico;
+
+
+
+public class AnalisadorSintatico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ analisadorsintatico/NaoTerminais.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.analisadorsintatico;
+
+
+public enum NaoTerminais implements Symbol {
+    Prog, LD, LDlinha, Decl, TipoVar, EA, EAlinha, TA, TAlinha, FA, ER, ERlinha, TR, OPBol, LC, LClinha, CMD, CMDAtrib, CMDEnt, CMDSaida, CMDCond, CMDCondlinha, CMDRep, SUB, Saida
+}
+
